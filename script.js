a3d8d641759553434ab4a23a1828016a77cb2937
larsanker50 <75078152+larsanker50@users.noreply.github.com>
2021-01-29 15:32:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let name = 'larsanker';
+let name = "Lars Anker"
 
-if(index.html.getElementById('button').clicked == true){
-   
+const h1 = document.getElementById("roep");
+const colors = ['red', 'green', 'blue', 'orange', 'yellow'];
+const button = document.getElementById('button');
+
+button.addEventListener("click", changeText);
+
+function changeText() {
+    h1.innerHTML = name;
+    h1.style.color = colors[Math.floor(Math.random() * colors.length)];
 }
